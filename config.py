f55628f3887a6ee60a6c03af86e97e6bdd204d78
jmitterh <jp86mitter@gmail.com>
2019-11-14 15:29:04 -0600

    Two ways to iterate through the api. appending to a list of columns and flatening deeply nested json page with the use of json normalize function

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # WEBSITE: https://developer.adzuna.com/signup
 # Interactive API: https://developer.adzuna.com/activedocs#!
 
-api_key = "ENTER API KEY"
-api_id = "ENTER API ID"
+api_key = "5f4837ff1aadf023edd24e8877d66677"
+api_id = "a9ccf687"
